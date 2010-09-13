6b452ea04f123a57ec6f7a33b916cf1c82ad0c18
Kivanio Barbosa <kivanio@gmail.com>
2010-09-13 16:53:23 -0400

    mudando testes para rspec2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,5 +2,5 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'brcobranca'
-require 'spec'
+require 'rspec'
 require 'tempfile'
