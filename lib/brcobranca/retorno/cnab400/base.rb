691b28a4f4713a80606a1320891066b5b9342236
Ewerton Miglioranza <efmiglioranza@gmail.com>
2015-07-16 15:42:54 -0300

    Fixed file read treatment for nil files.

--- lib/brcobranca/retorno/cnab400/base.rb
+++ lib/brcobranca/retorno/cnab400/base.rb
@@ -8,11 +8,9 @@ module Brcobranca
 
         # Load lines
         def self.load_lines(file, options={})
-          arquivo = File.open(file, "r")
-          codigo_banco = arquivo.gets[76..78]
-          arquivo.close
+          return nil if file.blank?
 
-          case codigo_banco
+          case codigo_banco_do_arquivo(file)
           when "237"
             Brcobranca::Retorno::Cnab400::Bradesco.load_lines(file, options)
 
@@ -23,8 +21,18 @@ module Brcobranca
             warn "Banco não encontrado (#{codigo_banco}). Carregando layout antigo padrão (ITAÚ)."
             Brcobranca::Retorno::RetornoCnab400.load_lines(file, options)
           end
-
         end
+
+        # Codigo do banco lido do arquivo.
+        # Registro Header [76..78]
+        def self.codigo_banco_do_arquivo(file)
+          arquivo = File.open(file, "r")
+          header = arquivo.gets
+          codigo_banco = header.blank? ? nil : header[76..78]
+          arquivo.close
+          codigo_banco
+        end
+
       end
     end
   end
