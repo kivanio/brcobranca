593e94b4ae25ca636a2e2e52ddfd8b027e54769a
Kivanio Barbosa <kivanio@gmail.com>
2015-09-09 14:27:54 -0400

    fix sicoob our number

--- lib/brcobranca/boleto/sicoob.rb
+++ lib/brcobranca/boleto/sicoob.rb
@@ -5,6 +5,7 @@ module Brcobranca
       validates_length_of :agencia, maximum: 4, message: "deve ser menor ou igual a 4 dígitos."
       validates_length_of :conta_corrente, maximum: 8, message: "deve ser menor ou igual a 8 dígitos."
       validates_length_of :numero_documento, maximum: 7, message: "deve ser menor ou igual a 7 dígitos."
+      validates_length_of :convenio, maximum: 7, message: 'deve ser menor ou igual a 7 dígitos.'
 
       def initialize(campos = {})
         campos = { carteira: "1" }.merge!(campos)
@@ -54,16 +55,38 @@ module Brcobranca
         "#{numero_documento}#{nosso_numero_dv}"
       end
 
-      # DV do nosso número seguindo o manual da sicoob
-      # http://www.bancoob.com.br/atendimentocobranca/CAS/2_Implantação_do_Serviço/Sistema_Proprio/DigitoVerificador.htm
+      # 3.13. Nosso número: Código de controle que permite ao Sicoob e à empresa identificar os dados da cobrança que deu origem ao boleto.
       #
-      # Os dígitos multiplicadores presentes na documentação da sicoob estão em ordem diferente da ordem abaixo
-      # devido ao cálculo em ordem inversa em Brcobranca::Calculo pelo método `multiplicador`.
+      # Para o cálculo do dígito verificador do nosso número, deverá ser utilizada a fórmula abaixo:
+      # Número da Cooperativa    9(4) – vide planilha "Capa" deste arquivo
+      # Código do Cliente   9(10) – vide planilha "Capa" deste arquivo
+      # Nosso Número   9(7) – Iniciado em 1
+      #
+      # Constante para cálculo  = 3197
+      #
+      # a) Concatenar na seqüência completando com zero à esquerda.
+      #     Ex.:Número da Cooperativa  = 0001
+      #           Número do Cliente  = 1-9
+      #           Nosso Número  = 21
+      #           000100000000190000021
+      #
+      # b) Alinhar a constante com a seqüência repetindo de traz para frente.
+      #     Ex.: 000100000000190000021
+      #          319731973197319731973
+      #
+      # c) Multiplicar cada componente da seqüência com o seu correspondente da constante e somar os resultados.
+      #     Ex.: 1*7 + 1*3 + 9*1 + 2*7 + 1*3 = 36
+      #
+      # d) Calcular o Resto através do Módulo 11.
+      #     Ex.: 36/11 = 3, resto = 3
+      #
+      # e) O resto da divisão deverá ser subtraído de 11 achando assim o DV (Se o Resto for igual a 0 ou 1 então o DV é igual a 0).
+      #     Ex.: 11 – 3 = 8, então Nosso Número + DV = 21-8
       #
       def nosso_numero_dv
         "#{agencia}#{convenio}#{numero_documento}".modulo11(
-          multiplicador: [3, 7, 9, 1],
-          mapeamento: { 10 => 0, 11 => 0 }
+          multiplicador: [3, 1, 9, 7],
+          mapeamento: { 1 => 0 }
         ) { |t| 11 - (t % 11) }
       end
 
