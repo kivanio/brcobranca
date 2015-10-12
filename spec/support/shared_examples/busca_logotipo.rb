2b8fcdc962c5819eeabc95b55c6357b93d67fe59
Gui Albuk <guilherme@n-pix.com>
2015-10-12 15:30:20 -0300

    Remove multibyte chars for ruby 1.9.3 compatibility

--- spec/support/shared_examples/busca_logotipo.rb
+++ spec/support/shared_examples/busca_logotipo.rb
@@ -1,11 +1,11 @@
 shared_examples_for 'busca_logotipo' do
-  it 'para layout padrão' do
+  it 'para layout padrao' do
     boleto_novo = described_class.new
     expect(File.exist?(boleto_novo.logotipo)).to be_truthy
     expect(File.stat(boleto_novo.logotipo).zero?).to be_falsey
   end
 
-  it 'para layout de carnê' do
+  it 'para layout de carne' do
     Brcobranca.configuration.gerador = :rghost_carne
     boleto_novo = described_class.new
     expect(File.exist?(boleto_novo.logotipo)).to be_truthy
