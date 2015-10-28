9f9a31ac2f5c72fbdb7aed492d8a05faffd43532
Dhyego Fernando <dhyegofernando@gmail.com>
2015-10-28 16:24:04 -0300

    feat(Retorno Cnab240): Add caixa retorno support

---
+++ lib/brcobranca/retorno/cnab240/base.rb
@@ -0,0 +1,37 @@
+# -*- encoding: utf-8 -*-
+require 'parseline'
+
+module Brcobranca
+  module Retorno
+    module Cnab240
+      class Base < Brcobranca::Retorno::Base
+
+        # Load lines
+        def self.load_lines(file, options={})
+          return nil if file.blank?
+
+          codigo_banco = codigo_banco_do_arquivo(file)
+
+          case codigo_banco
+          when "104"
+            Brcobranca::Retorno::Cnab240::Caixa.load_lines(file, options)
+
+          else
+            Brcobranca::Retorno::RetornoCnab240.load_lines(file, options)
+          end
+        end
+
+        # Codigo do banco lido do arquivo.
+        # Registro Header [0..2]
+        def self.codigo_banco_do_arquivo(file)
+          arquivo = File.open(file, "r")
+          header = arquivo.gets
+          codigo_banco = header.blank? ? nil : header[0..2]
+          arquivo.close
+          codigo_banco
+        end
+
+      end
+    end
+  end
+end
