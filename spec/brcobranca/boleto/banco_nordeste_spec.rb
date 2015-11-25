47007c4c5e20a6e58340f0ed811eb9707bbf6a25
Marcelo J. Both <marcelo.both@gmail.com>
2015-11-25 12:00:53 -0200

    Adiciona shared example para os formatos de arquivos

--- spec/brcobranca/boleto/banco_nordeste_spec.rb
+++ spec/brcobranca/boleto/banco_nordeste_spec.rb
@@ -112,35 +112,7 @@ RSpec.describe Brcobranca::Boleto::BancoNordeste do #:nodoc:[all]
     it_behaves_like 'busca_logotipo'
   end
 
-  it 'Gerar boleto nos formatos válidos com método to_' do
-    @valid_attributes[:data_documento] = Date.parse('2009/08/13')
-    boleto_novo = described_class.new(@valid_attributes)
-
-    %w(pdf jpg tif png).each do |format|
-      file_body = boleto_novo.send("to_#{format}".to_sym)
-      tmp_file = Tempfile.new('foobar.' << format)
-      tmp_file.puts file_body
-      tmp_file.close
-      expect(File.exist?(tmp_file.path)).to be_truthy
-      expect(File.stat(tmp_file.path).zero?).to be_falsey
-      expect(File.delete(tmp_file.path)).to eql(1)
-      expect(File.exist?(tmp_file.path)).to be_falsey
-    end
-  end
-
-  it 'Gerar boleto nos formatos válidos' do
-    @valid_attributes[:data_documento] = Date.parse('2009/08/13')
-    boleto_novo = described_class.new(@valid_attributes)
-
-    %w(pdf jpg tif png).each do |format|
-      file_body = boleto_novo.to(format)
-      tmp_file = Tempfile.new('foobar.' << format)
-      tmp_file.puts file_body
-      tmp_file.close
-      expect(File.exist?(tmp_file.path)).to be_truthy
-      expect(File.stat(tmp_file.path).zero?).to be_falsey
-      expect(File.delete(tmp_file.path)).to eql(1)
-      expect(File.exist?(tmp_file.path)).to be_falsey
-    end
+  describe 'Formato do boleto' do
+    it_behaves_like 'formatos_validos'
   end
 end
