156d5123348c6adbcfdddec259e96c524eb4c1fa
Marcelo J. Both <marcelo.both@gmail.com>
2015-12-15 17:29:05 -0200

    Corrige teste do código de barras

--- spec/brcobranca/boleto/banco_brasilia_spec.rb
+++ spec/brcobranca/boleto/banco_brasilia_spec.rb
@@ -52,7 +52,7 @@ RSpec.describe Brcobranca::Boleto::BancoBrasilia do #:nodoc:[all]
     expect { boleto_novo.codigo_barras }.not_to raise_error
     expect(boleto_novo.codigo_barras_segunda_parte).not_to be_blank
     expect(boleto_novo.codigo_barras_segunda_parte).to eql('0000820000528200000107013')
-    expect(boleto_novo.codigo_barras.linha_digitavel).to eql('07090.00087 20000.528206 00001.070135 9 66420000001000')
+    expect(boleto_novo.codigo_barras.linha_digitavel).to eql('07090.00087 20000.528206 00001.070135 4 66430000001000')
   end
 
   it 'Não permitir gerar boleto com atributos inválidos' do
