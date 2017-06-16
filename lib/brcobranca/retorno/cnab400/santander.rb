6d2bdfb808529cef51cdd2220fd686c1499c10d1
Douglas Araújo <daraujo@logfacil.com.br>
2017-06-16 08:40:36 -0300

    fix line size houndci-bot

--- lib/brcobranca/retorno/cnab400/santander.rb
+++ lib/brcobranca/retorno/cnab400/santander.rb
@@ -29,7 +29,7 @@ module Brcobranca
           # :conta, 22..30 #numero da conta corrente da empresa com digito
           parse.field :cedente_com_dv, 22..30
 
-          # :numero_controle_empresa, 37..61 #numero de controle do participante
+          # :numero_controle_empresa, 37..61 #numero de controle participante
 
           # :nosso_numero,62..69 # identificacao do titulo no banco
           parse.field :nosso_numero, 62..69
@@ -41,55 +41,57 @@ module Brcobranca
           # :cod_de_ocorrencia, 108..109 # cod de ocorrencia no banco
           parse.field :cod_de_ocorrencia, 108..109
 
-          # :data_de_ocorrencia, 110..115 # data de ocorrencia no banco (ddmmaa)
+          # :data_de_ocorrencia, 110..115 # data ocorrencia no banco (ddmmaa)
           parse.field :data_de_ocorrencia, 110..115
 
-          # :n_do_documento, 116..125 # numero do documento de cobranca (dupl, np etc)
+          # :n_do_documento, 116..125 # numero do documento de cobranca
           # :nosso_numero, 126..133 # nosso numero de novo?
           # :codigo_original_remessa, 134..135 # codigo original da remessa
 
-          parse.field :motivos_de_ocorrencia, 136..144, ->(motivos) { motivos.scan(/.../).reject { |n| n.to_i.zero? } }
+          parse.field :motivos_de_ocorrencia, 136..144, lambda do |motivos|
+            motivos.scan(/.../).reject { |n| n.to_i.zero? }
+          end
 
           # :brancos, 145..145 #complemento de registro
 
           # :vencimento, 146..151 #data de vencimento do titulo (ddmmaa)
           parse.field :data_vencimento, 146..151
 
-          # :valor_do_titulo, 152..164 #valor nominal do titulo (ultimos 2 digitos, virgula decimal assumida)
+          # :valor_do_titulo, 152..164 #valor nominal do titulo
           parse.field :valor_titulo, 152..164
 
-          # :codigo_do_banco, 165..167 # numero do banco na camara de compensacao
+          # :codigo_do_banco, 165..167 # numero do banco na compensacao
           parse.field :banco_recebedor, 165..167
 
-          # :agencia_cobradora, 168..171 # agencia cobradora, ag de liquidacao ou baixa
+          # :agencia_cobradora, 168..171 # agencia cobradora
           # :dac_ag_cobradora, 172..172 # dac da agencia cobradora
           parse.field :agencia_recebedora_com_dv, 168..172
 
           # :especie, 173..174 # especie do titulo
           parse.field :especie_documento, 173..174
 
-          # :tarifa_de_cobranca, 175..187 #valor da despesa de cobranca (ultimos 2 digitos, virgula decimal assumida)
+          # :tarifa_de_cobranca, 175..187 #valor da despesa de cobranca
           parse.field :valor_tarifa, 175..187
 
           # parse.field :outras_despesas, 188..200
           # :juros_operacao_em_atraso, 201..213 # zeros?
 
-          # :valor_do_iof, 214..226 #valor do iof a ser recolhido (ultimos 2 digitos, virgula decimal assumida)
+          # :valor_do_iof, 214..226 #valor do iof a ser recolhido
           parse.field :iof, 214..226
 
-          # :valor_abatimento, 227..239 #valor do abatimento concedido (ultimos 2 digitos, virgula decimal assumida)
+          # :valor_abatimento, 227..239 #valor do abatimento concedido
           parse.field :valor_abatimento, 227..239
 
-          # :descontos, 240..252 #valor do desconto concedido (ultimos 2 digitos, virgula decimal assumida)
+          # :descontos, 240..252 #valor do desconto concedido
           parse.field :desconto, 240..252
 
-          # :valor_principal, 253..265 #valor lancado em conta corrente (ultimos 2 digitos, virgula decimal assumida)
+          # :valor_principal, 253..265 #valor lancado em conta corrente
           parse.field :valor_recebido, 253..265
 
-          # :juros_mora_multa, 266..278 #valor de mora e multa pagos pelo sacado (ultimos 2 digitos, virgula decimal assumida)
+          # :juros_mora_multa, 266..278 #valor de mora multa pagos pelo sacado
           parse.field :juros_mora, 266..278
 
-          # :outros_creditos, 279..291 #valor de outros creditos (ultimos 2 digitos, virgula decimal assumida)
+          # :outros_creditos, 279..291 #valor de outros creditos
           parse.field :outros_recebimento, 279..291
 
           # :brancos, 292..292 #complemento de registro
@@ -102,15 +104,15 @@ module Brcobranca
           # :nome_do_sacado, 302..337 #Nome do sacado
           # :identificador_complemento, 338..338 #Identificador do Complemento
           # :unidade_moeda, 339..340 #Unidade de valor moeda corrente
-          # :valor_titulo_unidade, 341..353 #Valor do título em outra unidade de valor
-          # :valor_ioc_unidade, 354..366 #Valor do IOC em outra unidade de valor
-          # :valor_debito_credito, 367..379 #Valor do débito ou crédito
-          # :tipo_credito/débito (d/c), 380..380 #tipo crédito/débito (d/c)
+          # :valor_titulo_unidade, 341..353 #Valor do titulo unidade de valor
+          # :valor_ioc_unidade, 354..366 #Valor do IOC outra unidade de valor
+          # :valor_debito_credito, 367..379 #Valor do debito ou credito
+          # :tipo_credito/debito (d/c), 380..380 #tipo credito/debito (d/c)
           # :brancos, 381..383 #Brancos
           # :complemento, 384..385 #Complemento
           # :sigla_empresa, 386..389 #Sigla da empresa no sistema
           # :brancos, 390..391 #Brancos
-          # :numero_versão, 392..394 #Número da versão
+          # :numero_versao, 392..394 #Número da versao
 
           # :numero_sequencial, 394..399 #numero sequencial no arquivo
           parse.field :sequencial, 394..399
