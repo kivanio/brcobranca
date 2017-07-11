682632dda2fe8532b38db465766347172eac0864
AllanHorst <allanhorstwillig@gmail.com>
2017-07-11 15:28:58 -0300

    changed variable name

--- lib/brcobranca/remessa/cnab240/banco_brasil.rb
+++ lib/brcobranca/remessa/cnab240/banco_brasil.rb
@@ -6,7 +6,7 @@ module Brcobranca
       class BancoBrasil < Brcobranca::Remessa::Cnab240::Base
         # variacao da carteira
         attr_accessor :variacao
-        attr_accessor :tipo_movimento_remessa
+        attr_accessor :codigo_movimento_remessa
         # identificacao da emissao do boleto (attr na classe base)
         #   campo nao tratado pelo sistema do Banco do Brasil
         # identificacao da distribuicao do boleto (attr na classe base)
