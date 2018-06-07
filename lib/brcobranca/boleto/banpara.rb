9b26d50580ab597c74bde74d875b0a80e184860e
Poliana Santiago <polly.mags@gmail.com>
2018-06-07 16:51:31 -0300

    Ajusta geração do código de barras
    - Novo layout enviado pelo próprio banco banpara

--- lib/brcobranca/boleto/banpara.rb
+++ lib/brcobranca/boleto/banpara.rb
@@ -6,14 +6,11 @@ module Brcobranca
       validates_length_of :agencia, maximum: 4, message: 'deve ser menor ou igual a 4 dígitos.'
       validates_length_of :numero_documento, maximum: 11, message: 'deve ser menor ou igual a 11 dígitos.'
       validates_length_of :conta_corrente, maximum: 8, message: 'deve ser menor ou igual a 8 dígitos.'
-      validates_length_of :carteira, maximum: 4, message: 'deve ser menor ou igual a 4 dígitos.'
 
       # Nova instancia do Banpara
       # @param (see Brcobranca::Boleto::Base#initialize)
       def initialize(campos = {})
-        campos = { carteira: '06' }.merge!(campos)
-
-        campos[:local_pagamento] = 'Pagável em qualquer Banco até o vencimento'
+        campos[:local_pagamento] = 'Pagar preferencialmente em agência do Banpará'
 
         super(campos)
       end
@@ -45,9 +42,9 @@ module Brcobranca
 
       # Carteira
       #
-      # @return [String] 4 caracteres numéricos.
-      def carteira=(valor)
-        @carteira = valor.to_s.rjust(4, '0') if valor
+      # @return [String] CR de acordo com novo layout que banco enviou
+      def carteira
+        'CR'
       end
 
       # Conta corrente.
@@ -79,15 +76,6 @@ module Brcobranca
         ) { |total| 11 - (total % 11) }
       end
 
-      # Dígito verificador do nosso número
-      # @return [Integer] 1 caracteres numéricos.
-      def nosso_numero_dv
-        "#{carteira}#{numero_documento}".modulo11(
-          multiplicador: [2, 3, 4, 5, 6, 7],
-          mapeamento: { 10 => 'P', 11 => 0 }
-        ) { |total| 11 - (total % 11) }
-      end
-
       # Dígito verificador da conta corrente
       # @return [Integer] 1 caracteres numéricos.
       def conta_corrente_dv
@@ -100,24 +88,30 @@ module Brcobranca
       # Agência + conta corrente do cliente para exibir no boleto.
       # @return [String]
       # @example
-      #  boleto.agencia_conta_boleto #=> "0548 / 00001448-6"
+      #   Código da Agência = 0054
+      #   Conta de Cobrança= 6666666
+      #   Convênio=12345
+      #   Composição do Campo = 00546666666/12345
       def agencia_conta_boleto
-        "#{agencia} / #{conta_corrente}-#{conta_corrente_dv}"
+        "#{agencia}#{conta_corrente}/#{convenio}"
+      end
+
+      def nosso_numero_cod_barra
+        nosso_numero_boleto.to_s.rjust(13, '0')
       end
 
       # Segunda parte do código de barras.
       #
-      # Posição | Tamanho | Conteúdo<br/>
-      # 20 a 23 | 4       |  Agência Cedente (Sem o digito verificador, completar com zeros a esquerda quando  necessário)<br/>
-      # 24 a 27 | 4       |  Carteira<br/>
-      # 28 a 35 | 8       |  Número do Nosso Número(Sem o digito verificador)<br/>
-      # 36 a 43 | 8       |  Conta do Cedente (Sem o digito verificador, completar com zeros a esquerda quando necessário)<br/>
-      # 44 a 44 | 1       |  Zero<br/>
+      # NOVO LAYOUT Q O PROPRIO BANCO ENVIOU
+      # Posição | Tamanho | Conteúdo
+      # 20 a 26 | 7       |  fixo 0000999
+      # 27 a 31 | 5       |  Convenio
+      # 32 a 44 | 13      |  Nosso Numero com Zeros a esquerda
       #
       # @return [String] 25 caracteres numéricos.
 
       def codigo_barras_segunda_parte
-        "#{agencia}#{carteira}#{numero_documento}#{conta_corrente}0"
+        "0000999#{convenio}#{nosso_numero_cod_barra}"
       end
     end
   end
