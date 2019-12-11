eb714b8b2aab102ad8b0ac2567c5edc0bde2cebb
Mondrian Tecnologia <suporte.mondrian@gmail.com>
2019-12-11 16:26:36 -0300

    Update transferencia.rb

--- lib/brcobranca/remessa/transferencia.rb
+++ lib/brcobranca/remessa/transferencia.rb
@@ -23,24 +23,21 @@ module Brcobranca
       attr_accessor :agencia
       # <b>REQUERIDO</b>: conta do favorecido
       attr_accessor :conta
+      # <b>REQUERIDO</b>: digito conta do favorecido
+      attr_accessor :digito_conta
       # <b>REQUERIDO</b>: conta de conta CC ou PP
       attr_accessor :tipo_conta
 
       validates_length_of :banco, maximum: 3, message: 'deve ter 3 dígitos.'
       validates_length_of :agencia, maximum: 5, message: 'deve ter 5 dígitos.'
       validates_length_of :conta, maximum: 12, message: 'deve ter 12 dígitos.'
+      validates_length_of :digito_conta, maximum: 1, message: 'deve ter 1 dígitos.'
       validates_length_of :tipo_conta, maximum: 2, message: 'deve ter 2 dígitos.'
 
       def digito_agencia
         # utilizando a agencia com 4 digitos
         # para calcular o digito
         agencia.modulo11(mapeamento: { 10 => 'X' }).to_s
-      end
-
-      def digito_conta
-        # utilizando a conta corrente com 5 digitos
-        # para calcular o digito
-        conta.modulo11(mapeamento: { 10 => 'X' }).to_s
       end
 
       def codigo_finalidade
