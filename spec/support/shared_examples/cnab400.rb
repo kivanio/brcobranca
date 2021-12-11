ff6d1f7c95cafe7fe0a05de607a134939ddbbf43
Kivanio Barbosa <kivanio@gmail.com>
2021-12-11 16:03:10 -0400

    Fix codigo_beneficiario for Unicred

--- spec/support/shared_examples/cnab400.rb
+++ spec/support/shared_examples/cnab400.rb
@@ -1,20 +1,21 @@
-# -*- encoding: utf-8 -*-
+# frozen_string_literal: true
+
 shared_examples_for 'cnab400' do
   let(:pagamento) do
     Brcobranca::Remessa::Pagamento.new(valor: 199.9,
-      data_vencimento: Date.current,
-      nosso_numero: 123,
-      documento_sacado: '12345678901',
-      nome_sacado: 'PABLO DIEGO JOSÉ FRANCISCO,!^.?\/@  DE PAULA JUAN NEPOMUCENO MARÍA DE LOS REMEDIOS CIPRIANO DE LA SANTÍSSIMA TRINIDAD RUIZ Y PICASSO',
-      endereco_sacado: 'RUA RIO GRANDE DO SUL,!^.?\/@ São paulo Minas caçapa da silva junior',
-      bairro_sacado: 'São josé dos quatro apostolos magros',
-      cep_sacado: '12345678',
-      cidade_sacado: 'Santa rita de cássia maria da silva',
-      nome_avalista: 'ISABEL CRISTINA LEOPOLDINA ALGUSTA MIGUELA GABRIELA RAFAELA GONZAGA DE BRAGANÇA E BOURBON',
-      uf_sacado: 'SP')
+                                       data_vencimento: Date.current,
+                                       nosso_numero: 123,
+                                       documento_sacado: '12345678901',
+                                       nome_sacado: 'PABLO DIEGO JOSÉ FRANCISCO,!^.?\/@  DE PAULA JUAN NEPOMUCENO MARÍA DE LOS REMEDIOS CIPRIANO DE LA SANTÍSSIMA TRINIDAD RUIZ Y PICASSO',
+                                       endereco_sacado: 'RUA RIO GRANDE DO SUL,!^.?\/@ São paulo Minas caçapa da silva junior',
+                                       bairro_sacado: 'São josé dos quatro apostolos magros',
+                                       cep_sacado: '12345678',
+                                       cidade_sacado: 'Santa rita de cássia maria da silva',
+                                       nome_avalista: 'ISABEL CRISTINA LEOPOLDINA ALGUSTA MIGUELA GABRIELA RAFAELA GONZAGA DE BRAGANÇA E BOURBON',
+                                       uf_sacado: 'SP')
   end
   let(:params) do
-    if subject.class == Brcobranca::Remessa::Cnab400::Bradesco
+    if subject.instance_of?(Brcobranca::Remessa::Cnab400::Bradesco)
       { carteira: '01',
         agencia: '12345',
         conta_corrente: '1234567',
@@ -23,7 +24,7 @@ shared_examples_for 'cnab400' do
         sequencial_remessa: '1',
         codigo_empresa: '123',
         pagamentos: [pagamento] }
-    elsif subject.class == Brcobranca::Remessa::Cnab400::Citibank
+    elsif subject.instance_of?(Brcobranca::Remessa::Cnab400::Citibank)
       {
         portfolio: '17777751042700080112',
         carteira: '1',
@@ -31,7 +32,7 @@ shared_examples_for 'cnab400' do
         documento_cedente: '12345678910',
         pagamentos: [pagamento]
       }
-    elsif subject.class == Brcobranca::Remessa::Cnab400::Santander
+    elsif subject.instance_of?(Brcobranca::Remessa::Cnab400::Santander)
       {
         codigo_transmissao: '17777751042700080112',
         empresa_mae: 'SOCIEDADE BRASILEIRA DE ZOOLOGIA LTDA',
@@ -41,7 +42,7 @@ shared_examples_for 'cnab400' do
         digito_conta: '8',
         pagamentos: [pagamento]
       }
-    elsif subject.class == Brcobranca::Remessa::Cnab400::Sicoob
+    elsif subject.instance_of?(Brcobranca::Remessa::Cnab400::Sicoob)
       { carteira: '01',
         agencia: '1234',
         conta_corrente: '12345678',
@@ -50,7 +51,7 @@ shared_examples_for 'cnab400' do
         documento_cedente: '12345678910',
         convenio: '123456789',
         pagamentos: [pagamento] }
-    elsif subject.class == Brcobranca::Remessa::Cnab400::BancoBrasil
+    elsif subject.instance_of?(Brcobranca::Remessa::Cnab400::BancoBrasil)
       { carteira: '12',
         agencia: '1234',
         variacao_carteira: '123',
@@ -61,7 +62,7 @@ shared_examples_for 'cnab400' do
         documento_cedente: '12345678910',
         sequencial_remessa: '1',
         pagamentos: [pagamento] }
-    elsif subject.class == Brcobranca::Remessa::Cnab400::BancoNordeste
+    elsif subject.instance_of?(Brcobranca::Remessa::Cnab400::BancoNordeste)
       {
         carteira: '21',
         agencia: '1234',
@@ -71,7 +72,7 @@ shared_examples_for 'cnab400' do
         documento_cedente: '12345678910',
         pagamentos: [pagamento]
       }
-    elsif subject.class == Brcobranca::Remessa::Cnab400::Unicred
+    elsif subject.instance_of?(Brcobranca::Remessa::Cnab400::Unicred)
       {
         carteira: '03',
         agencia: '1234',
@@ -79,10 +80,10 @@ shared_examples_for 'cnab400' do
         digito_conta: '1',
         empresa_mae: 'SOCIEDADE BRASILEIRA DE ZOOLOGIA LTDA',
         documento_cedente: '12345678910',
-        codigo_transmissao: '12345678901234567890',
+        codigo_beneficiario: '12345678901234567890',
         pagamentos: [pagamento]
       }
-    elsif subject.class == Brcobranca::Remessa::Cnab400::Credisis
+    elsif subject.instance_of?(Brcobranca::Remessa::Cnab400::Credisis)
       {
         carteira: '18',
         agencia: '1234',
@@ -93,7 +94,7 @@ shared_examples_for 'cnab400' do
         documento_cedente: '12345678910',
         pagamentos: [pagamento]
       }
-    elsif subject.class == Brcobranca::Remessa::Cnab400::Banrisul
+    elsif subject.instance_of?(Brcobranca::Remessa::Cnab400::Banrisul)
       {
         carteira: '1',
         agencia: '1102',
@@ -134,23 +135,23 @@ shared_examples_for 'cnab400' do
 
     it 'informacoes devem estar posicionadas corretamente no trailer' do
       trailer = objeto.monta_trailer 3
-      expect(trailer[0]).to eq '9'                       # identificacao registro
+      expect(trailer[0]).to eq '9' # identificacao registro
 
-      if subject.class == Brcobranca::Remessa::Cnab400::Banrisul
+      if subject.instance_of?(Brcobranca::Remessa::Cnab400::Banrisul)
         expect(trailer[1..26]).to eq ''.rjust(26, ' ')      # brancos
         expect(trailer[27..39]).to eq '0000000019990'       # total geral
         expect(trailer[40..393]).to eq ''.rjust(354, ' ')   # brancos
-      elsif subject.class == Brcobranca::Remessa::Cnab400::Santander
+      elsif subject.instance_of?(Brcobranca::Remessa::Cnab400::Santander)
         expect(trailer[1..6]).to eq '000003'                # numero sequencial do registro
         expect(trailer[7..19]).to eq '0000000019990'        # total
         expect(trailer[20..393]).to eq ''.rjust(374, '0')   # zeros
-      elsif subject.class == Brcobranca::Remessa::Cnab400::Sicoob
+      elsif subject.instance_of?(Brcobranca::Remessa::Cnab400::Sicoob)
         expect(trailer[1..393]).to eq ''.rjust(393, '0')   # zeros
       else
         expect(trailer[1..393]).to eq ''.rjust(393, ' ')   # brancos
       end
 
-      expect(trailer[394..399]).to eq '000003'           # numero sequencial do registro
+      expect(trailer[394..399]).to eq '000003' # numero sequencial do registro
     end
   end
 
