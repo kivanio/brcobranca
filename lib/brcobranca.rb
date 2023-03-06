92ab988b1407bed8db10d38dc8e9d498198938e8
Kivanio Barbosa <kivanio@gmail.com>
2023-03-06 15:59:45 -0400

    Remove whitespace

--- lib/brcobranca.rb
+++ lib/brcobranca.rb
@@ -119,7 +119,7 @@ module Brcobranca
     module Template
       autoload :Base,        'brcobranca/boleto/template/base'
       autoload :Rghost,      'brcobranca/boleto/template/rghost'
-      autoload :Rghost2,     'brcobranca/boleto/template/rghost2'     
+      autoload :Rghost2,     'brcobranca/boleto/template/rghost2'
       autoload :RghostCarne, 'brcobranca/boleto/template/rghost_carne'
       autoload :RghostBolepix, 'brcobranca/boleto/template/rghost_bolepix'
     end
