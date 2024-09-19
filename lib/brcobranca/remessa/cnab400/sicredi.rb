c5ae8381c57af53c9906016229b4adb9098dfbc3
jhowk14 <jonathanperozi16@gmail.com>
2024-09-19 11:30:09 -0300

    initial implement sicredi cnab400

--- lib/brcobranca/remessa/cnab400/sicredi.rb
+++ lib/brcobranca/remessa/cnab400/sicredi.rb
@@ -1,15 +1,39 @@
+# frozen_string_literal: true
+
 module Brcobranca
   module Remessa
     module Cnab400
       class Sicredi < Brcobranca::Remessa::Cnab400::Base
-        attr_accessor :codigo_beneficiario, :numero_sicredi
+        # Código do beneficiário (informado pelo Sicredi no cadastramento)
+        attr_accessor :codigo_beneficiario
 
-        validates_presence_of :documento_cedente, :codigo_beneficiario
+        validates_presence_of :agencia, :conta_corrente, :documento_cedente,
+                              :digito_conta, :codigo_beneficiario,
+                              message: 'não pode estar em branco.'
+
+        validates_length_of :agencia, maximum: 4, message: 'deve ter 4 dígitos.'
+        validates_length_of :conta_corrente, maximum: 5, message: 'deve ter 5 dígitos.'
         validates_length_of :documento_cedente, minimum: 11, maximum: 14, message: 'deve ter entre 11 e 14 dígitos.'
+        validates_length_of :carteira, maximum: 2, message: 'deve ter 2 dígitos.'
+        validates_length_of :digito_conta, maximum: 1, message: 'deve ter 1 dígito.'
+
+        validates_inclusion_of :carteira, in: %w[1], message: 'não existente para este banco.'
 
         def initialize(campos = {})
-          campos = { aceite: 'N', codigo_beneficiario: '12345' }.merge!(campos)
+          campos = { aceite: 'N' }.merge!(campos)
           super(campos)
+        end
+
+        def agencia=(valor)
+          @agencia = valor.to_s.rjust(4, '0') if valor
+        end
+
+        def conta_corrente=(valor)
+          @conta_corrente = valor.to_s.rjust(5, '0') if valor
+        end
+
+        def carteira=(valor)
+          @carteira = valor.to_s.rjust(2, '0') if valor
         end
 
         def cod_banco
@@ -17,59 +41,124 @@ module Brcobranca
         end
 
         def nome_banco
-          'SICREDI'.ljust(15)
+          'SICREDI'.ljust(15, ' ')
+        end
+
+        def identificador_complemento
+          ' '
+        end
+
+        def nosso_numero(nosso_numero)
+          nosso_numero.to_s.rjust(8, '0')
+        end
+
+        def nosso_numero_dv(nosso_numero)
+          peso = [2, 3, 4, 5, 6, 7, 8, 9]
+          soma = nosso_numero.to_s.chars.reverse.each_with_index.map { |char, i| char.to_i * peso[i % 8] }.sum
+          dv = 11 - (soma % 11)
+          dv = 0 if dv == 10 || dv == 11
+          dv
+        end
+
+        def nosso_numero_boleto(nosso_numero)
+          "#{nosso_numero(nosso_numero)}#{nosso_numero_dv(nosso_numero)}"
+        end
+
+        def formata_nosso_numero(nosso_numero)
+          nosso_numero_boleto(nosso_numero).to_s
+        end
+
+        def codigo_beneficiario=(valor)
+          @codigo_beneficiario = valor.to_s.rjust(20, '0') if valor
         end
 
         def info_conta
-          codigo_beneficiario.to_s.rjust(5, '0')
+          codigo_beneficiario
         end
 
+        def complemento
+          codigo_beneficiario.rjust(277, ' ')
+        end
 
-        def data_geracao
-          Date.current.strftime('%Y%m%d')
+        def sequencial_remessa=(valor)
+          @sequencial_remessa = valor.to_s.rjust(7, '0') if valor
+        end
+
+        def digito_agencia
+          agencia.modulo11(mapeamento: { 10 => 'X' }).to_s
+        end
+
+        def digito_conta
+          conta_corrente.modulo11(mapeamento: { 10 => '0' }).to_s
+        end
+
+        def mapeamento_para_modulo_11
+          { 10 => 0, 11 => 0 }
         end
 
         def monta_header
-          cod = "1"
-          version = "2.00"
-          num_sequencial = 1.to_s.rjust(6, '0')
-          "01REMESSA01COBRANCA       #{info_conta}#{documento_cedente}                               #{cod_banco}#{nome_banco}#{data_geracao}        #{cod.to_s.rjust(7, '0')}                                                                                                                                                                                                                                                                                 #{version}#{num_sequencial}"
+          header = "01REMESSA01COBRANCA       #{info_conta}"
+          header += "#{empresa_mae.format_size(30)}#{cod_banco}"
+          header << "#{nome_banco}#{data_geracao}       000"
+          header << "#{sequencial_remessa}#{complemento}000001"
+          header
         end
-
-        def 
 
         def monta_detalhe(pagamento, sequencial)
-          detalhe = '1AAA H'
-          detalhe << ''.rjust(10, ' ')
-          detalhe << 'ABB'
-          detalhe << ''.rjust(28, ' ')
-          detalhe << pagamento.nosso_numero.to_s.rjust(9, '0')
-          detalhe << ''.rjust(6, ' ')
-          detalhe << pagamento.data_emissao.strftime('%y%m%d')
+          raise Brcobranca::RemessaInvalida, pagamento if pagamento.invalid?
 
-          # above is incorrect implementation
-          detalhe << pagamento.data_vencimento.strftime('%d%m%y')
-          detalhe << pagamento.valor.to_s.rjust(13, '0')
-          detalhe << cod_banco
-          detalhe << pagamento.especie_titulo
-          detalhe << aceite
-          detalhe << pagamento.cod_primeira_instrucao
-          detalhe << pagamento.formata_valor_mora
-          detalhe << pagamento.formata_valor_desconto
-          detalhe << pagamento.documento_sacado.to_s.rjust(14, '0')
-          detalhe << pagamento.nome_sacado.ljust(40)
-          detalhe << pagamento.endereco_sacado.ljust(40)
-          detalhe << pagamento.cep_sacado
-          detalhe << pagamento.cidade_sacado.ljust(15)
-          detalhe << pagamento.uf_sacado
-          detalhe << sequencial.to_s.rjust(6, '0')
+          detalhe = '1' # Código de transação
+          detalhe += agencia.rjust(4, '0') # Agência
+          detalhe << digito_agencia # Dígito da agência
+          detalhe << conta_corrente.rjust(8, '0') # Conta Corrente
+          detalhe << digito_conta # Dígito da conta
+          detalhe << ''.rjust(6, '0') # Zeros (filler)
+          detalhe << formata_nosso_numero(pagamento.nosso_numero).rjust(8, '0') # Nosso número
+          detalhe << '0' # Número da parcela (para Sicredi, pode ser 0)
+          detalhe << ' ' * 25 # Controle do participante (branco)
+          detalhe << cod_banco # Código do banco
+          detalhe << '00' # Zeros (filler)
+          detalhe << ''.rjust(25, ' ') # Brancos
+          detalhe << '2' # Código de multa (percentual)
+          detalhe << pagamento.formata_percentual_multa(10) # Valor da multa
+          detalhe << pagamento.tipo_mora # Tipo de mora
+          detalhe << 'N' # Aceite do título
+          detalhe << ' ' * 2 # Branco (filler)
+          detalhe << pagamento.identificacao_ocorrencia # Identificação da ocorrência (pagamento)
+          detalhe << pagamento.numero.to_s.rjust(10, '0') # Número do documento
+          detalhe << pagamento.data_vencimento.strftime('%d%m%y') # Data de vencimento
+          detalhe << pagamento.formata_valor.rjust(13, '0') # Valor do documento
+          detalhe << ''.rjust(10, '0') # Banco cobrador (zeros)
+          detalhe << pagamento.cod_desconto.rjust(2, '0') # Código do desconto
+          detalhe << pagamento.data_emissao.strftime('%d%m%y') # Data de emissão
+          detalhe << ''.rjust(2, '0') # Zeros (filler)
+          detalhe << pagamento.codigo_protesto # Código de protesto
+          detalhe << pagamento.dias_protesto.rjust(2, '0') # Dias para protesto
+          detalhe << pagamento.formata_valor_mora(13) # Valor mora ao dia
+          detalhe << pagamento.formata_data_desconto # Data limite para desconto
+          detalhe << pagamento.formata_valor_desconto.rjust(13, '0') # Valor do desconto
+          detalhe << formata_nosso_numero(pagamento.nosso_numero) # Nosso número formatado
+          detalhe << '00' # Zeros (filler)
+          detalhe << pagamento.formata_valor_abatimento(13) # Valor do abatimento
+          detalhe << pagamento.identificacao_sacado.rjust(2, '0') # Tipo de inscrição do sacado (CPF/CNPJ)
+          detalhe << pagamento.documento_sacado.to_s.rjust(14, '0') # Documento do sacado
+          detalhe << pagamento.nome_sacado.format_size(40) # Nome do sacado
+          detalhe << pagamento.endereco_sacado.format_size(40) # Endereço do sacado
+          detalhe << pagamento.bairro_sacado.format_size(12) # Bairro do sacado
+          detalhe << pagamento.cep_sacado.rjust(8, '0') # CEP do sacado
+          detalhe << pagamento.cidade_sacado.format_size(20) # Cidade do sacado
+          detalhe << pagamento.uf_sacado # UF do sacado
+          detalhe << pagamento.nome_avalista.format_size(38) # Nome do avalista
+          detalhe << sequencial.to_s.rjust(6, '0') # Número sequencial
           detalhe
         end
-
-        def monta_trailer(sequencial)
-          "9#{sequencial.to_s.rjust(6, '0')}#{''.rjust(374, '0')}#{sequencial.to_s.rjust(6, '0')}"
+        def monta_trailer(quantidade_registros)
+          trailer = "9" # Código do trailer
+          trailer << ''.rjust(393, '0') # Filler com zeros
+          trailer << quantidade_registros.to_s.rjust(6, '0') # Número sequencial
+          trailer
         end
-      end
+      end  
     end
   end
 end
