58b2a65d63f37df20fcba0f8d44892b5315a575f
wilfison <wilfisonbatista@gmail.com>
2026-02-16 15:00:35 -0300

    Adiciona testes para verificação de logotipo em diferentes layouts, incluindo suporte para Prawn.

--- spec/support/shared_examples/busca_logotipo.rb
+++ spec/support/shared_examples/busca_logotipo.rb
@@ -3,6 +3,8 @@
 shared_examples_for 'busca_logotipo' do
   it 'para layout padrão' do
     boleto_novo = described_class.new
+
+    expect(boleto_novo.logotipo).to match(/\.eps\z/)
     expect(File).to exist(boleto_novo.logotipo)
     expect(File.stat(boleto_novo.logotipo)).not_to be_zero
   end
@@ -10,7 +12,21 @@ shared_examples_for 'busca_logotipo' do
   it 'para layout de carnê' do
     Brcobranca.configuration.gerador = :rghost_carne
     boleto_novo = described_class.new
+
+    expect(boleto_novo.logotipo).to match(/\.eps\z/)
     expect(File).to exist(boleto_novo.logotipo)
     expect(File.stat(boleto_novo.logotipo)).not_to be_zero
+  end
+
+  it 'para layout com prawn' do
+    Brcobranca.configuration.gerador = :prawn
+    boleto_novo = described_class.new
+
+    expect(boleto_novo.logotipo).to match(/\.png\z/)
+    expect(File).to exist(boleto_novo.logotipo)
+    expect(File.stat(boleto_novo.logotipo)).not_to be_zero
+
+    # Restaura o gerador para o valor padrão após os testes
+    Brcobranca.configuration.gerador = :rghost
   end
 end
